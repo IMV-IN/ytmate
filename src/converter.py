c62b4a0270b3654c3596b0e971e3c998e09032e9
Yuvraj Singh Chauhan <ysinghcin@gmail.com>
2026-03-18 20:32:01 +0530

    Used ffmpeg python bindings for merging streams instead of relying on subprocess
    First one solved on to the next

--- src/converter.py
+++ src/converter.py
@@ -4,12 +4,11 @@ import subprocess as sbp
 def convert(video, audio, title):
     input_video = ffm.input(video)
     input_audio = ffm.input(audio)
+    output_file = f"{title}.mp4"
 
     try:
-        convert = sbp.run(["ffmpeg" ,"-i" ,f"{video}" ,"-i" ,f"{audio}" ,"-c" ,"copy" ,f"{title}" ,"-hide_banner"])
-        if convert != 0:
-            raise RuntimeError
-    except:
-        ffm.concat(input_video, input_audio, v=1, a=1).output(title+".mp4").run()
-        if convert != 0:
-            return -1
+        ffm.output(input_video, input_audio, output_file, vcodec='copy', acodec='copy').run()
+    except ffm.Error as e:
+        print(f"Error occurred during conversion: {e}")
+        return -1
+    return 0
